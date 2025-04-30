39dfa0e5eca1d3b647fca04e1557cc2d948ae49c
Jesus David Huamani Arrieta <jesushuamanicuc@gmail.com>
2025-04-30 18:26:47 -0500

    Agregando archivo index.php con código Hola Mundo

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// Función de conexión MySQL con SSL
-function getMySqlConnection() {
-    $host = "10.175.0.4";  // Asegúrate de que esta IP sea accesible
-    $user = "jbase";
-    $pass = "Aliceyeloy2473";
-    $db   = "main";
-    $port = 3306;
-
-    $con = mysqli_connect($host, $user, $pass, $db, $port);
-
-    if (!$con) {
-        die("<div class='message error'>❌ Error MySQL (SSL): " . mysqli_connect_error() . "</div>");
-    }
-    return $con;
-}
-
-// Llamada a la función
-$conn = getMySqlConnection();
-echo "Conexión exitosa!";
+echo "¡Hola, mundo!";
 ?>
-
- 
