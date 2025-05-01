c78148929b456f21d9db9681572b6cd24300d075
Jesus David Huamani Arrieta <jesushuamanicuc@gmail.com>
2025-04-30 19:24:35 -0500

    Actualización de conexión a la base de datos en index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "¡Hola, mundo!";
+$host = 'basededatosflexible1.mysql.database.azure.com';
+$user = 'jbase';
+$password = 'Aliceyeloy2473';
+$database = 'rubricadb'; // <--- nombre correcto de tu base de datos
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "✅ Conexión exitosa a la base de datos.";
+$conn->close();
 ?>
+
